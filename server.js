928a1e9ed8e216d0b63381653d4efd4f2ff0e0e0
rahul8588 <rahulthegamester@gmail.com>
2016-09-21 16:53:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,69 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var articleOne = {
+    title :'Article one: Rahul Kumar',
+    heading :'Article one',
+    date :'21/09/2016',
+    content :` <p>
+                  This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. 
+             </p>
+              
+              <p>
+                  This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. 
+              </p>
+              <p>
+                  This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. This is the content for my Article one page. 
+              </p> `
+
+};
+function createTemplate (data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.teading;
+    var content=data.contant
+
+var htmlTemplete= `
+   <html>
+       <head>
+    <title>
+        ${title}
+  </title>
+  <meta name="viewport" content="width-device-width, initial-scale-1" />
+  <link href="/ui/style.css" rel="stylesheet" />
+  <style>
+    
+  </style>
+      </head> 
+      <body>
+          <div class="container">
+         <div>
+         <a href="/">Home</a>
+         
+          </div> 
+          <hr>
+          <h3>
+              ${heading}
+          </h3>
+          <div>
+             ${date}
+          </div>
+          <div>
+             ${content}
+             
+              
+          </div>
+          </div>
+      </body>
+    </html>
+     `;
+          return htmlTemplate;
+}
+
+
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req,res){
