0ec2426ceaeb36da46106f0d3c4b7f569c41d6b0
rahul8588 <rahulthegamester@gmail.com>
2016-09-21 22:31:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
-img.onClick = function() {
-    log.style.marginLeft = '100px';
+img.onClick = function () {
+    img.style.marginLeft = '100px';
 };
