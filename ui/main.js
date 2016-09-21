520cf467fab2f5f8d599df39b09c0ef64182c8ad
rahul8588 <rahulthegamester@gmail.com>
2016-09-21 22:22:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('nody');
+img.onClick = function() {
+    log.style.marginLeft = '100px';
+};
