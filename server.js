763e258c735444d6a3a3cc61db3804d9feb79f38
rahul8588 <rahulthegamester@gmail.com>
2016-09-21 14:43:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one', function (req,res){
-    res.send("Artical one requested and will be served here");
+app.get('/article-one', function (req,res){
+    res.send("Article one requested and will be served here");
 });
 
-app.get('/artical-two', function (req,res){
-    res.send("Artical two requested and will be served here");
+app.get('/article-two', function (req,res){
+    res.send("Article two requested and will be served here");
 });
-app.get('/artical-three', function (req,res){
-    res.send("Artical three requested and will be served here");
+app.get('/article-three', function (req,res){
+    res.send("Article three requested and will be served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
