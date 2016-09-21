9b20e1da537843227a167faab705e6d006b625bf
rahul8588 <rahulthegamester@gmail.com>
2016-09-21 21:19:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ var htmlTemplate= `
 }
 
 
-app.get('/articleName', function (req,res){
+app.get('/:articleName', function (req,res){
     var articleName= req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
